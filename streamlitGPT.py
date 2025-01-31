55e7b9f49177fb5ad21236e995afafd13ed504b8
Leticia Eltermann <140015770+leticiael@users.noreply.github.com>
2025-01-31 03:01:02 -0300

    Update streamlitGPT.py
    Atualizando o projeto ChatGPT Streamlit: Personalizado para agendar consultas médicas.

--- streamlitGPT.py
+++ streamlitGPT.py
@@ -1,7 +1,7 @@
 import openai
 import streamlit as st
 
-st.title("ChatGPT like")
+st.title("Marcar consulta")
 
 openai.api_key = st.secrets["OPENAI_API_KEY"]
 
@@ -11,12 +11,22 @@ if "openai_model" not in st.session_state:
 if "messages" not in st.session_state:
     st.session_state["messages"] = []
 
+
+
 for message in st.session_state["messages"]:
     with st.chat_message(message["role"]):
         st.markdown(message["content"])
 
-if prompt := st.chat_input("O que você quer saber hoje?"):
-    instructions = "Responda as perguntas de maneira ágil"
+if prompt := st.chat_input("Fale sobre sua consulta"):
+    instructions = (
+    "Você é um assistente virtual de uma clínica médica, responsável por agendar consultas. "
+    "Sempre seja educado, objetivo e profissional. "
+    "Ao iniciar a conversa, cumprimente o paciente e pergunte se deseja marcar uma consulta. "
+    "Se ele responder afirmativamente, peça informações como plano de saúde, data e local desejado. "
+    "Se o plano for Notredame, informe que ele não é aceito. Caso contrário, ofereça a opção de consulta particular. "
+    "Evite repetir perguntas já respondidas pelo paciente e confirme todos os detalhes no final da conversa. "
+    "Nunca peça informações irrelevantes e mantenha um tom profissional e acolhedor."
+)
 
     st.session_state.messages.append({"role": "user", "content": prompt})
     
@@ -27,10 +37,10 @@ if prompt := st.chat_input("O que você quer saber hoje?"):
         client = openai.OpenAI()  
         stream = client.chat.completions.create(
             model=st.session_state['openai_model'],
-            messages=[
-                {"role": "system", "content": instructions},
-                {"role": "user", "content": prompt}
-            ],
+           messages=[
+    {"role": "system", "content": instructions}
+] + st.session_state["messages"] + [{"role": "user", "content": prompt}]
+,
             stream=True
         )
         response = st.write_stream(stream)
